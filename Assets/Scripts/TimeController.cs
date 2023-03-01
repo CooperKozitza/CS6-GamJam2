79d24abf3820b4e9f00b393b73270fc0689c6ee0
Jake <jakereiss5096@gmail.com>
2023-03-01 13:15:52 -0800

    Trees and time
    Added treetops and clock

---
+++ Assets/Scripts/TimeController.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TimeController : MonoBehaviour
+{
+
+    [SerializeField]
+    private float timeMultiplier;
+
+    [SerializeField]
+    private float startingHour;
+
+    [SerializeField]
+    private TextMeshProUGUI timeDisplay;
+
+    private DateTime currentTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = DateTime.Now.Date + TimeSpan.FromHours(startingHour);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateTimeOfDay();
+    }
+
+    private void UpdateTimeOfDay()
+    {
+        currentTime = currentTime.AddSeconds(Time.deltaTime * timeMultiplier);
+
+        if (timeDisplay != null)
+        {
+            timeDisplay.text = currentTime.ToString("HH:mm");
+        }
+    }
+}
