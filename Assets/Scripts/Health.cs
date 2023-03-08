7d0705a06855f09aeec2ff2636ce5ba18e5df150
Jake <jakereiss5096@gmail.com>
2023-03-08 12:52:20 -0800

    Added short health script

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+
+    [SerializeField]
+    int playerHP = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Acid"))
+        {
+            playerHP--;
+            Debug.Log(playerHP);
+            if (playerHP == 0)
+            {
+                Object.Destroy(gameObject);
+            }
+        }
+    }
+}
